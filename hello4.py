8dd67868c680f30e1416c17ce543dd8d7ceb53f1
KrishnaTechy <43444535+KrishnaHacky@users.noreply.github.com>
2019-09-18 15:44:42 +0530

    Add files via upload

---
+++ hello4.py
@@ -0,0 +1,49 @@
+import os,sys,sysconfig,webbrowser,time,datetime
+greetings =""#insert greetings into it by time and other things
+#look out on youtube
+#HOW TO autorun scripts on starup
+#how to set anaconda as interpreter
+sent1 = "what you wanna do sir"
+for x in sent1:
+    sys.stdout.write(x.upper())
+    sys.stdout.flush()
+    time.sleep(0.04)
+time.sleep(0.6)
+sys.stdout.write("\b"*21)
+for x in "give me a task :":
+    if x==":":
+        sys.stdout.write(x)
+        var1 =input()
+        break
+    sys.stdout.write(x.upper())
+    sys.stdout.flush()
+    time.sleep(0.05)
+print("my service is that only but it is in working")
+time.sleep(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+try:
+    pass
+finally:
+    print("bye")
+    time.sleep(1)
+    os.open("C:\\Users\\krishna\\Desktop\\gaane\\Alan_Walker_‒_Play_(Lyrics)_ft._K-391,_Tungevaag,_Mangoo(256k).mp3",2)
